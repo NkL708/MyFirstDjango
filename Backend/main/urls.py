e167453c539c682e6b611bff71a3ed13e85f5eae
NkL <nklnsk708@gmail.com>
2022-06-26 16:57:59 +0700

    get fail? Fix it

--- Backend/main/urls.py
+++ Backend/main/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path, include
 from .api import UserViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('api/users', UserViewSet, 'users')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls))
+]
